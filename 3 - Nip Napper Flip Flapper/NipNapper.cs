dbc8f3eabc028b0d16e118a15b45f16647ef7e23
anton <antontj@live.dk>
2022-04-18 14:06:59 +0200

    Version 1

---
+++ 3 - Nip Napper Flip Flapper/NipNapper.cs
@@ -0,0 +1,62 @@
+﻿namespace Nip_Napper;
+
+public class NipNapper
+{
+    private int currentLayer;
+    private int currentState;
+    private Dictionary<int, string> texts;
+
+    public NipNapper()
+    {
+        currentLayer = 1;
+        currentState = 1;
+        texts = new()
+        {
+            {1, "Aglet 1"},
+            {2, "Aglet 2"},
+            {3, "Aglet 3"},
+            {4, "Aglet 4"},
+            {5, "Aglet 5"},
+            {6, "Aglet 6"},
+            {7, "Aglet 7"},
+            {8, "Aglet 8"}
+        };
+    }
+
+    public string[] GetFields()
+    {
+        switch (currentLayer)
+        {
+            case 1:
+                return new[] {"1", "2", "3", "4"};
+            default:
+                switch (currentState)
+                {
+                    case 1:
+                        return new[] {"1", "2", "5", "6"};
+                    default:
+                        return new[] {"3", "4", "7", "8"};
+                }
+        }
+    }
+
+    public string ChooseField(string field)
+    {
+        var num = int.Parse(field);
+        switch (currentLayer)
+        {
+            case 1:
+            case 2:
+                FlipFlap(num);
+                return "";
+            default:
+                return texts[num];
+        }
+    }
+
+    private void FlipFlap(int count)
+    {
+        currentLayer++;
+        currentState = (currentState + count) % 2;
+    }
+}
---
+++ 3 - Nip Napper Flip Flapper/Program.cs
@@ -0,0 +1,28 @@
+﻿using Nip_Napper;
+
+var game = new NipNapper();
+
+var input = GetInput();
+game.ChooseField(input);
+input = GetInput();
+game.ChooseField(input);
+input = GetInput();
+var output = game.ChooseField(input);
+
+Console.WriteLine(output);
+
+string GetInput()
+{
+    while (true)
+    {
+        Console.WriteLine("Choose a field:");
+        var options = String.Join(", ", game.GetFields());
+        Console.WriteLine(options);
+
+        var input = Console.ReadLine();
+        if (game.GetFields().Contains(input))
+        {
+            return input;
+        }
+    }
+}
